e0fa850318802df0ef14439378c5a91aa4002b6d
= <2022pcecrnikhil040@poornima.org>
2025-01-04 19:51:55 +0530

    Emloyee Management System using React

---
+++ ems/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {
+      // Add your customizations here
+    },
+  },
+  plugins: [
+    // Add Tailwind CSS plugins here, e.g., require('@tailwindcss/forms')
+  ],
+};
